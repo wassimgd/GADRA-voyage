5db77f03febd72a73ddc4afb5eb6394b42dfb5aa
wassimgd <130101480+wassimgd@users.noreply.github.com>
2023-05-31 21:44:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded",()=>{
+    const swiper = new Swiper('.home-slider',{
+        loop:true,
+        //navigation arrows 
+        navigation:{
+            nextEl:'.swiper-button-next',
+            prevEl:'.swiper-button-prev',
+        },
+        })
+   
+    
+})
